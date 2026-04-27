d07f06bc4dfece2960e4152b9ced112806083e0d
jpedropraf <joaopeedropradofrancsico18@gmail.com>
2026-04-27 18:15:26 -0300

    config fastifyAdapter in main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  
+  const adapter = new FastifyAdapter({
+    logger: true, 
+    bodyLimit: 1048576, 
+    caseSensitive: true, 
+    connectionTimeout: 30000,
+  });
+
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    adapter,
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Users Api')
@@ -15,6 +30,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 void bootstrap();
