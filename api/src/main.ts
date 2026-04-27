48975c69ccda989a284d393ee826ea45876ebf1a
jpedropraf <joaopeedropradofrancsico18@gmail.com>
2026-04-27 11:09:43 -0300

    refactor(core): replace express with fastify for better performance:Replace ExpressAdapter with FastifyAdapter in main.ts,Update request types in UsersController to use FastifyRequest,Update dependencies to include @nestjs/platform-fastify

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Users Api')
